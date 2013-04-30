ecf38efc37caf4b04d8e27d143bc3e7aba3b920b
John Mark Schofield <jms@schof.org>
2013-04-30 15:35:33 -0700

    Bumping version number.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
     pass
 
 tests_require = [
-    'Django>=1.2,<1.5',
+    'Django>=1.2',
     'django-nose',
     'nose',
     'South',
@@ -23,7 +23,7 @@ install_requires = [
 
 setup(
     name='gargoyle',
-    version='0.10.6a1',
+    version='0.10.6a2',
     author='DISQUS',
     author_email='opensource@disqus.com',
     url='http://github.com/disqus/gargoyle',
